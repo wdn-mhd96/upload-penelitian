42f948c99c60d01d7823b8ae5999578f36186811
muhamad-bari <barsid589@gmail.com>
2023-08-07 10:56:31 +0700

    judul fix

--- dosen/log_book.php
+++ dosen/log_book.php
@@ -25,87 +25,81 @@
           <button type="submit" class="btn btn-primary" name="tampil">Buka Logbook</button>
         </div>
       </form>
-      <?php 
-            include '../../koneksi/koneksi.php';
-            if(isset($_POST['tampil']))
-            {
-              $id = $_POST['judul'];
-              $query = mysqli_query($koneksi, "SELECT * from logbook2 left JOIN surat_mhs on logbook2.nim = surat_mhs.nim where id_penelitian='$id' and Id='$id'");
-              if(mysqli_num_rows($query)>0)
-              {
-              $b = mysqli_fetch_array($query);
-              // echo "<script>window.location  = 'log_book.php?id=".$b['id_logbook']."';</script>";
-              $a = "<a href='tambah_log.php?id=".$b['id_logbook']."' class='btn btn-primary float-left mt-2'>Tambah Log</a>";
-              $c = "<b>".$b['judul']."</b>";
-              mysqli_data_seek($query, 0);
-              }
-              else
-              {
-                $query = mysqli_query($koneksi, "SELECT * from logbook_header left join surat_mhs on logbook_header.nim = surat_mhs.nim where id_penelitian='$id' and Id='$id'");
-                $b = mysqli_fetch_array($query);
-                // echo "<script>window.location  = 'log_book.php?id=".$b['id_logbook']."';</script>";
-                $a = "<a href='tambah_log.php?id=".$b['id_logbook']."' class='btn btn-primary float-left mt-2'>Tambah Log</a>";
-                $c = "<b>".$b['judul'] ."</b>";
-              
-              }
-            }
-          }
-            ?>
+      <?php
+      include '../../koneksi/koneksi.php';
+      if (isset($_POST['tampil'])) {
+        $id = $_POST['judul'];
+        $query = mysqli_query($koneksi, "SELECT * from logbook2 left JOIN surat_mhs on logbook2.nim = surat_mhs.nim where id_penelitian='$id' and Id='$id'");
+        if (mysqli_num_rows($query) > 0) {
+          $b = mysqli_fetch_array($query);
+          // echo "<script>window.location  = 'log_book.php?id=".$b['id_logbook']."';</script>";
+          $a = "<a href='tambah_log.php?id=" . $b['id_logbook'] . "' class='btn btn-primary float-left mt-2'>Tambah Log</a>";
+          $c = "<b>" . $b['judul'] . "</b>";
+          mysqli_data_seek($query, 0);
+        } else {
+          $query = mysqli_query($koneksi, "SELECT * from logbook_header left join surat_mhs on logbook_header.nim = surat_mhs.nim where id_penelitian='$id' and Id='$id'");
+          $b = mysqli_fetch_array($query);
+          // echo "<script>window.location  = 'log_book.php?id=".$b['id_logbook']."';</script>";
+          $a = "<a href='tambah_log.php?id=" . $b['id_logbook'] . "' class='btn btn-primary float-left mt-2'>Tambah Log</a>";
+          $c = "<b>" . $b['judul'] . "</b>";
+        }
+      }
+      ?>
       <br>
       <div class="card mb-4">
-          <div class="card text-center">
-            <div class="card-header">
-              <?= $c ?>
-            </div>
-            <?php include 'data-logbook.php'?>
+        <div class="card text-center">
+          <div class="card-header">
+            <?= $c ?>
           </div>
-          </div>
+          <?php include 'data-logbook.php' ?>
         </div>
       </div>
     </div>
-  </main>
-  <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-  <!--import jquery datatable -->
-  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script>
+</div>
+</div>
+</main>
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<!--import jquery datatable -->
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script>
 
-  <script>
-    $(document).ready(function() {
-      $('#datalogbook').DataTable({
-        "processing": true,
-        "serverSide": true,
-        "ajax": "log_data.php?id=<?= $b['id_logbook'] ?>",
+<script>
+  $(document).ready(function() {
+    $('#datalogbook').DataTable({
+      "processing": true,
+      "serverSide": true,
+      "ajax": "log_data.php?id=<?= $b['id_logbook'] ?>",
 
-        "order": [
-          [0, 'asc']
-        ],
+      "order": [
+        [0, 'asc']
+      ],
 
-        // membuat kolom
-        "columns": [
+      // membuat kolom
+      "columns": [
 
-          //untuk membuat data index
-          {
-            data: 'no',
-            name: 'id',
-            render: function(data, type, row, meta) {
-              return meta.row + meta.settings._iDisplayStart + 1;
-            }
-          },
+        //untuk membuat data index
+        {
+          data: 'no',
+          name: 'id',
+          render: function(data, type, row, meta) {
+            return meta.row + meta.settings._iDisplayStart + 1;
+          }
+        },
 
-          //samakan data kolom sesuai dt di data.php
-          {
-            "data": 'nim'
-          },
-          {
-            "data": 'isi_logbook'
-          },
-          {
-            "data": 'tanggal'
-          },
-          {
-            "data": 'progress'
-          },
-        ]
-      });
+        //samakan data kolom sesuai dt di data.php
+        {
+          "data": 'nim'
+        },
+        {
+          "data": 'isi_logbook'
+        },
+        {
+          "data": 'tanggal'
+        },
+        {
+          "data": 'progress'
+        },
+      ]
     });
-  </script>
-  <?php include_once "footer.php"; ?>
+  });
+</script>
+<?php include_once "footer.php"; ?>
