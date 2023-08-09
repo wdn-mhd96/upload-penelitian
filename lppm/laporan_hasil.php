ac551019eed96436bc0d16ca4e9cfae53d6eb828
wdn-112 <wildan.mhd.96@gmail.com>
2023-08-09 11:02:33 +0700

    laporan hasil

--- lppm/laporan_hasil.php
+++ lppm/laporan_hasil.php
@@ -7,18 +7,33 @@
         <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
         <li class="breadcrumb-item active">Status Berkas Saya</li>
       </ol>
+      <form action="" method="post">
+        <div class="form-row ml-2 mb-2">
+          <label for="" class="mt-2 mr-2">Jenis Laporan : </label>
+          <select name="jenis" class="form-control w-25 mr-3">
+            <option value=""></option>
+            <option value="penelitian">Penelitian</option>
+            <option value="pengmas">Pengabdian Masyarakat</option>
+          </select>
+          <button type="submit" class="btn btn-success" name="pilih">Buka Laporan</button>
+        </div>
+      </form>
+      <div class="card mb-4">
       <?php
       include "../../koneksi/koneksi.php";
+      if(isset($_POST['pilih']))
+      {
       $no = 1;
       $gg = $_SESSION['username'];
-      $sql = "SELECT * from laporan2 order by jenis_laporan desc";
+      $jenis = $_POST['jenis'];
+      $sql = "SELECT * from laporan2 where jenis_laporan='$jenis'";
       $query = mysqli_query($koneksi, $sql) or die(mysqli_error());
-      // $ada=mysqli_query($koneksi, $sql) or die(mysqli_error());                                                            
+      // $ada=mysqli_query($koneksi, $sql) or die(mysqli_error());    
+      }                                                        
       ?>
-      <div class="card mb-4">
         <div class="card text-center">
           <div class="card-header">
-            Daftar Laporan Hasil
+            Daftar Laporan Hasil <?= $jenis ?>
           </div>
           <div class="card-body">
             <table class="table table-bordered" id="datadosesn" width="100%" cellspacing="0">
@@ -26,14 +41,21 @@
                 <tr>
                   <th>No.</th>
                   <th>Judul Laporan</th>
-                  <th>Jenis Laporan</th>
                   <th>Nama Dosen</th>
                   <th>Lampiran</th>
                   <th>Tanggal</th>
                   <th>Status</th>
+                  <th>Aksi</th>
                 </tr>
               </thead>
-              <?php while ($row = mysqli_fetch_array($query)) { ?>
+              
+              <?php 
+              if(!$query)
+              {
+                echo "Pilih Jenis Laporan";
+              }
+              else{
+                while ($row = mysqli_fetch_array($query)) { ?>
                 <tbody>
                   <tr>
                     <td><?php echo $no++; ?></td>
@@ -43,7 +65,6 @@
                     ?>
                       <td><?php echo $row['judul']; ?></td>
                     <?php } ?>
-                    <td><?php echo $row['jenis_laporan']; ?></td>
                     <td><?php echo $row['nama']; ?></td>
                     <?php if ($row['file_laporan'] == '') { ?>
                       <td>Lampiran Belum di Upload</td>
@@ -57,9 +78,10 @@
                     ?>
                       <td>File Sudah Di Upload</td>
                     <?php } ?>
+                    <td><a onclick="return confirm('yakin ingin hapus data?')" href="hapus_laporan.php?id=<?= $row['id_laporan'] ?>" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Hapus Laporan"><i class="fas fa-trash"></i></a></td>
                   </tr>
                 </tbody>
-              <?php } ?>
+              <?php } } ?>
             </table>
 
 
